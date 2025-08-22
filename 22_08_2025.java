2baa5cd2a0a7f27b4db75a000a1c36f87cee9ce6
PUSHPINDER SINGH <84889778+pushpinder2001@users.noreply.github.com>
2025-08-22 16:20:53 +0530

    Create 22_08_2025.java
    again zero day

---
+++ 22_08_2025.java
@@ -0,0 +1,55 @@
+You are given a 2D binary array grid. Find a rectangle with horizontal and vertical sides with the smallest area, such that all the 1's in grid lie inside this rectangle.
+
+Return the minimum possible area of the rectangle.
+
+ 
+
+Example 1:
+
+Input: grid = [[0,1,0],[1,0,1]]
+
+Output: 6
+
+Explanation:
+
+
+
+The smallest rectangle has a height of 2 and a width of 3, so it has an area of 2 * 3 = 6.
+
+Example 2:
+
+Input: grid = [[1,0],[0,0]]
+
+Output: 1
+
+Explanation:
+
+
+
+The smallest rectangle has both height and width 1, so its area is 1 * 1 = 1.
+
+ 
+
+Constraints:
+
+1 <= grid.length, grid[i].length <= 1000
+grid[i][j] is either 0 or 1.
+The input is generated such that there is at least one 1 in grid.
+  Solution:
+class Solution {
+    public int minimumArea(int[][] grid) {
+        int minrow=grid.length,maxrow=-1;
+        int mincol=grid[0].length,maxcol=-1;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    minrow=Math.min(minrow,i);
+                    maxrow=Math.max(maxrow,i);
+                    mincol=Math.min(mincol,j);
+                    maxcol=Math.max(maxcol,j);
+                }
+            }
+        }
+        return (maxrow-minrow+1)*(maxcol-mincol+1);
+    }
+}
