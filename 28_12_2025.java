e9a541441ca06480247a1544f914f97011409836
PUSHPINDER SINGH <84889778+pushpinder2001@users.noreply.github.com>
2025-12-28 18:12:38 +0530

    Add solution to count negatives in a matrix
    Implemented a solution to count negative numbers in a sorted matrix.

---
+++ 28_12_2025.java
@@ -0,0 +1,44 @@
+Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
+
+ 
+
+Example 1:
+
+Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+Output: 8
+Explanation: There are 8 negatives number in the matrix.
+Example 2:
+
+Input: grid = [[3,2],[1,0]]
+Output: 0
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 100
+-100 <= grid[i][j] <= 100
+ 
+
+Follow up: Could you find an O(n + m) solution?
+
+  Solution:
+
+  class Solution {
+    public int countNegatives(int[][] grid) {
+        int m=grid.length,n=grid[0].length;
+        int i=m-1,j=0;
+
+        int res=0;
+
+        while(i>=0 && j<n){
+            if(grid[i][j]<0){
+                res+=n-j;
+                i--;
+            }else
+            j++;
+        }
+        return res;
+    }
+}
