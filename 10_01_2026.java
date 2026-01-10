36e62d8a3376c91d2c21d32b0f28fad49e562631
PUSHPINDER SINGH <84889778+pushpinder2001@users.noreply.github.com>
2026-01-10 07:52:21 +0530

    Add minimumDeleteSum method for string comparison
    Implement a solution to find the minimum ASCII sum of deleted characters to make two strings equal.

---
+++ 10_01_2026.java
@@ -0,0 +1,48 @@
+Given two strings s1 and s2, return the lowest ASCII sum of deleted characters to make two strings equal.
+
+ 
+
+Example 1:
+
+Input: s1 = "sea", s2 = "eat"
+Output: 231
+Explanation: Deleting "s" from "sea" adds the ASCII value of "s" (115) to the sum.
+Deleting "t" from "eat" adds 116 to the sum.
+At the end, both strings are equal, and 115 + 116 = 231 is the minimum sum possible to achieve this.
+Example 2:
+
+Input: s1 = "delete", s2 = "leet"
+Output: 403
+Explanation: Deleting "dee" from "delete" to turn the string into "let",
+adds 100[d] + 101[e] + 101[e] to the sum.
+Deleting "e" from "leet" adds 101[e] to the sum.
+At the end, both strings are equal to "let", and the answer is 100+101+101+101 = 403.
+If instead we turned both strings into "lee" or "eet", we would get answers of 433 or 417, which are higher.
+ 
+
+Constraints:
+
+1 <= s1.length, s2.length <= 1000
+s1 and s2 consist of lowercase English letters.
+
+  Solution:
+
+class Solution {
+    public int minimumDeleteSum(String s1, String s2) {
+        int m=s1.length(),n=s2.length();
+        int[][] dp=new int[m+1][n+1];
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(s1.charAt(i)==s2.charAt(j)){
+                    dp[i][j]=s1.charAt(i)+dp[i+1][j+1];
+                }else{
+                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        int total=0;
+        for(char c:s1.toCharArray())total+=c;
+        for(char c:s2.toCharArray())total+=c;
+    return total-2*dp[0][0];
+    }
+}
